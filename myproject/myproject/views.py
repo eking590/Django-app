f151686a3461f2d382d1601d04ed3a071b87438e
ebor <eborodirikingsley@gmail.com>
2025-12-05 23:34:26 +0100

    first commit

---
+++ myproject/myproject/views.py
@@ -0,0 +1,12 @@
+#from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    #return HttpResponse("Welcome to the Home Page!")
+    return render(request, 'home.html')
+
+
+def about(request):
+    #return HttpResponse("This is the About Page.")
+    return render(request, 'about.html')
+

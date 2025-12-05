2a0180dfced7c3ca399276cb261bea1773471da5
ebor <eborodirikingsley@gmail.com>
2025-12-05 23:58:23 +0100

    new changes

--- myproject/posts/urls.py
+++ myproject/posts/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [   
     # path('', views.posts_list),
-    path('', views.posts_list),
+    path('', views.posts_list), 
+    path('<int:post_id>/', views.post_detail), 
+
 ]
 
